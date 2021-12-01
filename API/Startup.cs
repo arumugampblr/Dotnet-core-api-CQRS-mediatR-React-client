05ac084456dc7ba73307209dc2803e8db287ac14
Pugalendi Arumugam <89121032+arumugampblr@users.noreply.github.com>
2021-12-01 12:07:18 +0530

    EOS 5 crud completed React client side

--- API/Startup.cs
+++ API/Startup.cs
@@ -9,15 +9,17 @@ using Microsoft.EntityFrameworkCore;
 using Persistence;
 using MediatR;
 using Application.Activities;
+using Application.Core;
+using API.Extensions;
 
 namespace API
 {
     public class Startup
     {
-        private readonly IConfiguration config;
+        private readonly IConfiguration _config;
         public Startup(IConfiguration config)
         {
-            this.config = config;
+            _config = config;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -25,24 +27,7 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt=>
-            {
-                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddCors(opt=>{
-                opt.AddPolicy("CorsPolicy",policy=>
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });           
-            } );
-
-            services.AddMediatR(typeof(List.Handler).Assembly);
-
+            services.AddApplicationServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
