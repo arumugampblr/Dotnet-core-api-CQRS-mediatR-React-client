1258e5ecbb3b814749e7e2b2e80f7ffc4c55c474
Pugalendi Arumugam <89121032+arumugampblr@users.noreply.github.com>
2021-12-04 20:23:50 +0530

    React routing implemented

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,5 +1,6 @@
 import { observer } from "mobx-react-lite";
 import React, { SyntheticEvent, useState } from "react";
+import { Link } from "react-router-dom";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 
@@ -30,6 +31,7 @@ export default observer(function ActivityList(){
                              <div>{activity.city}, {activity.venue}</div>
                          </Item.Description>
                          <Item.Extra>
+                             <Button as={Link} to={`/activities/${activity.id}`} floated='right' content='View' color='blue'/>
                              <Button
                                     name={activity.id} 
                                     loading={loading && target === activity.id} 
@@ -37,7 +39,6 @@ export default observer(function ActivityList(){
                                     floated='right' 
                                     content='Delete' 
                                     color='red'/>
-                             <Button onClick={()=>activityStore.selectActivity(activity.id)} floated='right' content='View' color='blue'/>
                              <Label basic content={activity.category}/>
                          </Item.Extra>
                      </Item.Content>
